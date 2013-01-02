29d47c95f748f15c17b708ed39a952bdabf91088
Assaf Shomer <assafshomer@gmail.com>
2013-01-03 00:52:42 +0200

    included test helper for testing email case insensitivity

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,4 @@
 module UsersHelper
-
-	def reverse_string_case(string)
-		reversed=string
-		i=0
-		string.each_char do |char|		
-			if char==char.upcase
-				reversed[i]=char.downcase
-			else
-				reversed[i]=char.upcase
-			end
-			i+=1
-		end
-		reversed
-	end
-
+	
 end
+
---
+++ spec/support/test_utilities.rb
@@ -0,0 +1,15 @@
+module TestUtilities
+	def reverse_string_case(string)
+		reversed=string
+		i=0
+		string.each_char do |char|		
+			if char==char.upcase
+				reversed[i]=char.downcase
+			else
+				reversed[i]=char.upcase
+			end
+			i+=1
+		end
+		reversed
+	end
+end
