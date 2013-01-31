8197f569eecd2253caff043b22a26055cff9e4a8
Assaf Shomer <assafshomer@gmail.com>
2013-01-31 13:43:08 +0200

    added length contstraint on microposts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -35,8 +35,24 @@ describe Micropost do
   		expect do
   		  Micropost.new(content: "lorem", user_id: 1)
   		end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)  		
+  	end  	
+  end
+
+  describe "content" do
+  	describe "should not be blank" do
+  		before { @micropost.content="   "}
+  		it { should_not be_valid }  		
+  	end
+  	
+  	describe "should be less than 140 chars" do
+  		before { @micropost.content= "a" * 141 }
+  		it { should_not be_valid }
   	end
 
-  	
+  	describe "shoule be able to be 140 chars long" do
+  		before { user.microposts.create(content: "b"*140) }
+  		it { should be_valid }  		
+  	end
   end
+
 end 
