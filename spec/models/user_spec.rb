3a2f4e2678bc5c2103aa5d5d6315cd7a20a8728f
Assaf Shomer <assafshomer@gmail.com>
2013-02-07 13:32:04 +0200

    added relationship model and association to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,7 @@ describe "User" do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
 
 	it { should be_valid } 
 	it { should_not be_admin }
@@ -183,6 +184,11 @@ describe "User" do
 			its(:feed) { should_not include(unfollowed_post) }
 
 		end
+	end
+
+	describe "relationships" do
+
+		
 	end
 
 end 
