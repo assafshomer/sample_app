52dfb565fd88f8dd4b795057348ecdb69aec8dc7
Assaf Shomer <assafshomer@gmail.com>
2013-04-21 11:04:16 +0300

    Implement remember me for deciding whether a permanent cookie is saved upon signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		@title ='Sign in'
 		user=User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in user
+			sign_in user, params[:session][:remember_me]
 			flash[:success]="Welcome back #{user.name}"
 			redirect_back_or user 
 		else
