9b39ca545e4a3ad3b2e59e66f1a5507e292734ca
Assaf Shomer <assafshomer@gmail.com>
2013-04-25 15:01:41 +0300

    Dedicated MailersHelper method for password reset email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @password_reset=@user.password_resets.build  	
       if @password_reset.save!
-        notify(@user.email,"password reset token : #{@password_reset.password_reset_token}" ) if @user
+        send_password_reset_email(@password_reset) if @user
         flash[:success] = "A password reset link was sent to #{@user.email}"
         redirect_to root_path
       end  
--- app/helpers/mailers_helper.rb
+++ app/helpers/mailers_helper.rb
@@ -1,12 +1,18 @@
 module MailersHelper
 
 	def send_new_follower_email_to(user)
-		subject="Hello #{@user.name}, #{current_user.name} is now following you"
+		subject="Hello #{user.name}, #{current_user.name} is now following you"
 		notify(user.email,subject)
 	end
 
 	def send_user_stopped_following_email_to(user)
-		subject="Hello #{@user.name}, #{current_user.name} is no longer following you"
+		subject="Hello #{user.name}, #{current_user.name} is no longer following you"
+		notify(user.email,subject)
+	end
+
+	def send_password_reset_email(password_reset)
+		subject="password reset token : #{password_reset.password_reset_token}"
+		user=User.find_by_id(password_reset.user_id)
 		notify(user.email,subject)
 	end
 	
