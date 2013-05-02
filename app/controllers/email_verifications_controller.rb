f9a50faff063d3eb8aa64e6f170378d870822718
Assaf Shomer <assafshomer@gmail.com>
2013-05-02 16:07:16 +0300

    Still need to write tests for signup with email verification and to write the code that will make the signin tests pass

--- app/controllers/email_verifications_controller.rb
+++ app/controllers/email_verifications_controller.rb
@@ -2,16 +2,10 @@ class EmailVerificationsController < ApplicationController
   def edit
   	@title="Email verification"
   	@email_verification=EmailVerification.find_by_token(params[:id])
-  	@user=@email_verification.user    
-  	@email_verification.toggle!(:active)
-  	if @email_verification.active?
-  		
-  	else
-  		
-  	end
-  	@user.toggle!(:active)
-	  # sign_in @user
-	  # flash[:success] = "Welcome to my Twitter clone"
-	  # redirect_to @user  	
+  	@user=@email_verification.user      	
+  	@user.toggle!(:active) unless @user.active?
+	  sign_in @user
+	  flash[:success] = "Welcome to my Twitter clone"
+	  redirect_to @user  	
   end
 end
