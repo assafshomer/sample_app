f52f5931901f8662edd3c3e5f04e35adf59a2c06
Assaf Shomer <assafshomer@gmail.com>
2013-05-06 11:01:31 +0300

    Made choice for case insensitive search depend on the database type using the production env variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,11 @@ module ApplicationHelper
     search_terms=extract_minimal_search_terms(space_separated_terms.split)
     wrap_percent=search_terms.map {|term| "%#{term}%" } 
     field_names.each do |field|
-      append_like=["#{field} LIKE ? "]*search_terms.size
+    	if Rails.configuration.database_configuration[Rails.env]["adapter"] =~ /postgresql/
+    		append_like=["#{field} ILIKE ? "]*search_terms.size	
+    	else
+    		append_like=["#{field} LIKE ? "]*search_terms.size
+    	end      
       sql_query+=append_like.join(" OR ") + " OR "
     end
     sql_query=sql_query[0,sql_query.length-' OR '.length]
