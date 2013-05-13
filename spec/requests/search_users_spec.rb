991e2811e450f0551e57b4e3132339d651fbe964
Assaf Shomer <assafshomer@gmail.com>
2013-05-13 09:33:41 +0300

    Add following search tests

--- spec/requests/search_users_spec.rb
+++ spec/requests/search_users_spec.rb
@@ -13,7 +13,7 @@ describe "Search Users" do
     test_sign_in user    
   end
 
-  describe "search users" do
+  describe "search users on the users list" do
     before { visit users_path }
     it { should have_selector('input#search') }          
     it { should have_selector('input#users_search_button') }
@@ -24,8 +24,7 @@ describe "Search Users" do
       it { should have_selector('div.pagination') }          
     end
     describe "clear users search" do      
-      before do
-        visit users_path              
+      before do        
         fill_in 'search', with: 'blahhhh'
         click_button 'Search'                                
         click_button 'Clear'                    
@@ -33,31 +32,31 @@ describe "Search Users" do
       it "should find marlon, marlo and bill" do
         # page.should have_selector('li', text: marlon.name)
       end                             
-    end              
+    end
+    describe "should find a user by name" do
+      before do        
+        fill_in 'search', with: 'marlo a.x'
+        click_button 'Search'
+      end
+      it "should find marlon, marlo and bill" do
+        page.should have_selector('li', text: marlon.name)
+        page.should have_link 'Email' , href: "mailto:#{marlon.email}"
+        page.should have_link(marlon.name, href: user_path(marlon.id))
+        page.should have_selector('li', text: marlo.name)
+        page.should have_link 'Email' , href: "mailto:#{marlo.email}"
+        page.should have_link(marlo.name, href: user_path(marlo.id))
+        page.should have_selector('li', text: bill.name)
+        page.should have_link 'Email' , href: "mailto:#{bill.email}"
+        page.should have_link(bill.name, href: user_path(bill.id))              
+      end
+      it "should not find barack" do
+        page.should_not have_selector('li', text: barack.name)
+        page.should_not have_link 'Email' , href: "mailto:#{barack.email}"
+        page.should_not have_link(barack.name, href: user_path(barack.id))                                      
+      end                      
+    end                   
   end
-  describe "should find a user by name" do
-    before do
-      visit users_path              
-      fill_in 'search', with: 'marlo a.x'
-      click_button 'Search'
-    end
-    it "should find marlon, marlo and bill" do
-      page.should have_selector('li', text: marlon.name)
-      page.should have_link 'Email' , href: "mailto:#{marlon.email}"
-      page.should have_link(marlon.name, href: user_path(marlon.id))
-      page.should have_selector('li', text: marlo.name)
-      page.should have_link 'Email' , href: "mailto:#{marlo.email}"
-      page.should have_link(marlo.name, href: user_path(marlo.id))
-      page.should have_selector('li', text: bill.name)
-      page.should have_link 'Email' , href: "mailto:#{bill.email}"
-      page.should have_link(bill.name, href: user_path(bill.id))              
-    end
-    it "should not find barack" do
-      page.should_not have_selector('li', text: barack.name)
-      page.should_not have_link 'Email' , href: "mailto:#{barack.email}"
-      page.should_not have_link(barack.name, href: user_path(barack.id))                                      
-    end                      
-  end 
+
   describe "search followers" do   
     let!(:follower) { FactoryGirl.create(:user) }
     before do
@@ -99,7 +98,49 @@ describe "Search Users" do
         # page.should_not have_link(barack.name, href: user_path(barack.id))   # this dude fails due to the avatar link                      
       end                     
     end       
-  end          
+  end
+  describe "search following" do   
+    let!(:followed_user) { FactoryGirl.create(:user) }
+    before do
+      user.follow!(followed_user)
+      visit following_user_path(user)
+    end
+    it { should have_selector('input#search') }          
+    it { should have_selector('input#users_search_button') }
+    it { should have_selector('input#users_clear_search_button') }
+    describe 'should not filter on empty search' do
+      before do 
+        User.all.each {|tempuser| user.follow!(tempuser) if tempuser.name=='testuser'}
+        click_button 'Search'
+      end
+      it { should have_selector('div.pagination') }          
+    end        
+
+    describe "should find a user by name" do
+      before do
+        user.follow!(marlo)
+        user.follow!(marlon)
+        user.follow!(bill)
+        user.follow!(barack)
+        fill_in 'search', with: 'marlo a.x'
+        click_button 'Search'        
+      end
+      it "should find marlon, marlo and bill but not barack" do
+        page.should have_selector('li', text: marlon.name)
+        page.should have_link 'Email' , href: "mailto:#{marlon.email}"
+        page.should have_link(marlon.name, href: user_path(marlon.id))
+        page.should have_selector('li', text: marlo.name)
+        page.should have_link 'Email' , href: "mailto:#{marlo.email}"
+        page.should have_link(marlo.name, href: user_path(marlo.id))
+        page.should have_selector('li', text: bill.name)
+        page.should have_link 'Email' , href: "mailto:#{bill.email}"
+        page.should have_link(bill.name, href: user_path(bill.id))
+        page.should_not have_selector('li', text: barack.name)
+        page.should_not have_link 'Email' , href: "mailto:#{barack.email}"
+        # page.should_not have_link(barack.name, href: user_path(barack.id))   # this dude fails due to the avatar link                      
+      end                     
+    end       
+  end              
 end
 
 
